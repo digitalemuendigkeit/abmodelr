37630e5f340b6943f5434f9a6c9b0c18f6c3ee1f
Nils Plettenberg <nils.plettenberg@rwth-aachen.de>
2019-05-13 13:35:57 +0200

    merge conflict

--- generate_cosine_matrix.R
+++ generate_cosine_matrix.R
@@ -1,13 +1,21 @@
 #generate cosine matrix to compare user interest to news posts
+veclength <- function(x){
+  sqrt(x %*% x)
+}
+
 
 generate_cosine_matrix <- function(user, news_posts){
-  user %>% select(starts_with("topic")) -> mat_user
-  news_posts %>% select(starts_with("topic")) -> mat_posts
+  
+  user %>% select(starts_with("topic")) %>% as.matrix() -> mat_user
+  news_posts %>% select(starts_with("topic"))%>% as.matrix() -> mat_posts
   cosine_matrix <- matrix(c(0), nrow = config$n_users, ncol = total_newsposts)
-  for(i in 1:config$n_users) {
-    for(j in 1:total_newsposts) {
-      cosine_matrix[i,j] <- lsa::cosine(unlist(mat_user[i,]), unlist(mat_posts[j,]))
-    }
-  }
+  
+  # rowwise normalization to simplify cosine
+  mat_user_norm <-  diag(1/apply(mat_user, 1, veclength)) %*% mat_user 
+  mat_post_norm <- diag(1/apply(mat_posts, 1, veclength)) %*% mat_posts 
+
+  # now only matrix mult  
+  cosine_matrix <- mat_user_norm %*% t(mat_post_norm)
+  
   cosine_matrix
 }
